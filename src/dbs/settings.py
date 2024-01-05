9408bde69d5284ac68180c0817e715f75956d45c
Benjamin Dornel <benjamindornel@gmail.com>
2024-01-05 23:20:48 +0800

    feat: raise error if any .env vars are missing

--- src/dbs/settings.py
+++ src/dbs/settings.py
@@ -2,15 +2,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class CloudSettings(BaseSettings):
-    dbs_user_id: str = ""
-    dbs_pin: str = ""
-    project_id: str = ""
-    secret_id: str = ""
+    dbs_user_id: str
+    dbs_pin: str
+    project_id: str
+    secret_id: str
     trusted_user_emails: list = []
-    otp_email_subject: str = ""
-    bucket_name: str = ""
-    from_email: str = ""
-    to_email: str = ""
+    otp_email_subject: str
+    bucket_name: str
+    from_email: str
+    to_email: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
